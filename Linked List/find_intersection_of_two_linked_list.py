3e2d14bd652340129c3bf573fc0f9975766fee04
charulagrawal <charul.agrawal@pagalguy.com>
2015-06-07 13:23:47 +0530

    Few corrections

--- Linked List/find_intersection_of_two_linked_list.py
+++ Linked List/find_intersection_of_two_linked_list.py
@@ -1,3 +1,7 @@
+# -*- coding: UTF-8 -*-
+
+# Program to find intersection of two sorted linked list
+
 import initialize 
 
 def find_intersection_of_two_linked_list(head1, head2):
@@ -12,16 +16,14 @@ def find_intersection_of_two_linked_list(head1, head2):
 			current1 = current1.nextnode
 		elif current2.data < current1.data:
 			current2 = current2.nextnode
-		elif current1.nextnode == current2.nextnode:
-			current1 = current1.nextnode
-			current2 = current2.nextnode
-			return "Linked list intersect at %s"%str(current1.nextnode.data)
+		elif current1.data == current2.data:
+			return "Linked list intersect at %s"%str(current1.data)
 			
 
 	return 'Lists do not intersect'
 
 
-head1 = initialize.initialize_linked_list_by_array([30,15,9,6, 3])
-head2 = initialize.initialize_linked_list_by_array([30, 15, 10])
+lList1 = initialize.initialize_linked_list_by_array([30,15,9,6, 3])
+lList2 = initialize.initialize_linked_list_by_array([30, 15, 10])
 
-print find_intersection_of_two_linked_list(head1, head2)
+print find_intersection_of_two_linked_list(lList1.head, lList2.head)
