c67a88ac5d550914d437fa183c4112991945087d
charulagrawal <charul.agrawal@pagalguy.com>
2015-06-11 13:12:01 +0530

    Changes so that it works for all kinds of graph

--- Graphs/hamiltonian_cycle.py
+++ Graphs/hamiltonian_cycle.py
@@ -2,15 +2,15 @@
 
 # Program to find hamiltonian cycle in a graph
 
-import graph 
+import graph
 
-def isSafe(graph, path, v, i):
-	if i in graph.edges[v] and i not in path:
+def isSafe(graph, path, i):
+	if i in graph.edges[path[-1]] and i not in path:
 		return True
 	return False
 
-def hamiltonian_cycle_util(graph, path, v):
-	if v == graph.V-1:
+def hamiltonian_cycle_util(graph, path):
+	if len(path) == graph.V:
 		if 0 in graph.edges[path[-1]]:
 			path.append(0)
 			print path
@@ -19,16 +19,16 @@ def hamiltonian_cycle_util(graph, path, v):
 			return False
 	else:
 		for i in range(graph.V):
-			if isSafe(graph, path, v, i):
-				path.append(v)
-				if hamiltonian_cycle_util(graph, path, v+1):
+			if isSafe(graph, path, i):
+				path.append(i)
+				# Recursive look for solution
+				if hamiltonian_cycle_util(graph, path):
 					return True
 				# Backtrack
 				del path[-1]
 
 		return False
 
-
 graph = graph.construct_graph_undirected()
 
-print hamiltonian_cycle_util(graph, [0], 1)
+print hamiltonian_cycle_util(graph, [0])
