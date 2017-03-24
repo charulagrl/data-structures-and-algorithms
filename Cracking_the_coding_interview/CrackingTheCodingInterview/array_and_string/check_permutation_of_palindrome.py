216581039789b1e68491714c8f4ea8d410dc8ff3
charulagrl <charul.agrl@gmail.com>
2017-03-25 01:04:36 +0530

    Solutions to problems in Cracking the coding interview book

---
+++ Cracking_the_coding_interview/CrackingTheCodingInterview/array_and_string/check_permutation_of_palindrome.py
@@ -0,0 +1,91 @@
+# -*- coding: UTF-8 -*-
+
+'''
+
+	Problem: Given a string, check if it is a permutation of palindromic String. The palindrome does not 
+	need to be limited to just dictionary words. 
+
+	Input: Tact Coa
+	Output: True (permuatations: "taco cat")
+'''
+
+'''
+	Approach 1: Get all permutation of a string and check for each if its a palindrome or not
+
+	Time Complexity: O(n!) where n is the length of the string
+'''
+
+# TODO: Write a program to get all the permutation of a string
+
+
+'''
+
+	Approach 2: Store all the characters of a string in a hash map and then check in the hash map if any two characters comes 
+	odd number of times, return False
+
+	Time Complexity: O(n) Space Complexity: O(n)
+'''
+def permuation_palindrome_string(s):
+
+	# the upper-lower case letter will be treated same
+	s = s.lower()
+	# spaces in the string does not matter
+	s = s.replace(" ", "")
+
+	hash_map = {}
+
+	for c in s:
+		if c not in hash_map:
+			hash_map[c] = 1
+		else:
+			hash_map[c] += 1
+
+
+	flag = False
+	for key, value in hash_map.iteritems():
+		if flag and value % 2 != 0:
+			return False
+
+		if value % 2 != 0:
+			flag = True
+		
+
+	return True
+
+'''
+
+	Any string of even length should have all even counts of characters in order to be a palindrome. A string of odd length should 
+	have only one character with odd count. So we just need to make sure that there is only one such character. We can avoid one 
+	loop in the above solution by counting the number of odd-even with each character value.
+
+	Time Complexity: O(n) Space Complexity: O(n)
+'''
+
+def permutation_palindrome_string_2(s):
+
+	# Consider upper and lower case characters as same
+	s = s.lower()
+	# Spaces are not considered
+	s = s.replace(" ", "")
+
+	hash_map = {}
+	count_odd = 0
+
+	for c in s:
+
+		# Check and store in hash_map
+		if c not in hash_map:
+			hash_map[c] = 1
+		else:
+			hash_map[c] += 1
+
+		# check for the count each time
+		if hash_map[c] % 2 == 0:
+			count_odd -=1
+		else:
+			count_odd += 1
+
+	if count_odd <= 1:
+		return True
+
+	return False
---
+++ Cracking_the_coding_interview/CrackingTheCodingInterview/array_and_string/check_permutation_strings.py
@@ -0,0 +1,65 @@
+# -*- coding: UTF-8 -*-
+
+'''
+
+	Problem: Given two strings, check if one string is a permutation of another. 
+
+	Naive Approach: Get all combinations of one string and match with the other string, if it is similar or not.
+
+	Time Complexity: O(n!) where n is the length of the string.
+
+	Better Solution: Store all the characters of a string in a hash-map and keep the count of each character across each key. 
+	While parsing through the other string, reduce the count for each character in hash_map. If the final hash_map is empty, both
+	the strings are permutation of each other.
+
+	Time Complexity: O(n) where n is the length of string
+	Space Complexity: O(n) 
+
+'''
+
+
+# function to check if two strings are permutation of each other
+def check_permutation(s1, s2):
+
+	hash_map = {}
+
+	for i in s1:
+
+		if i not in hash_map:
+			hash_map[i] = 1
+
+		else:
+			hash_map[i] += 1
+
+
+	# loop through the second string
+	for i in s2:
+		# delete the value for each character found in hash_map
+		if i in hash_map:
+			if hash_map[i] > 1:
+				hash_map[i] -= 1
+
+			else:
+				del hash_map[i]
+
+		# if any character is not found in hash map, return False
+		else:
+			return False
+
+
+	# check if the hash_map is empty
+	if len(hash_map) == 0:
+		return True
+	else:
+		return False
+
+# Writing unit-test for this function
+import unittest
+
+class MyTest(unittest.TestCase):
+
+	def test(self):
+		self.assertEqual(check_permutation("aabcbaah"), "aabcbaah")
+		self.assertEqual(check_permutation("aabcccccaaa"), "a2b1c5a3")
+
+unittest.main()
