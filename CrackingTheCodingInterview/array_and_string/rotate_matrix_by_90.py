1b6d8ff0d4bef714fcc5cf4949fae983b1d1cfaf
charulagrl <charul.agrl@gmail.com>
2017-03-25 13:23:52 +0530

    Program to rotate a matrix by 90

--- CrackingTheCodingInterview/array_and_string/rotate_matrix_by_90.py
+++ CrackingTheCodingInterview/array_and_string/rotate_matrix_by_90.py
@@ -12,15 +12,41 @@
 
 '''
 
-def rotate(matrix):
+'''
+	Approach 1: By taking an additional 2-D matrix and copying the elements from original matrix
 
+	Complexity: O(n * 2) Space Complexity: O(n * 2)
+'''
+def rotate_90(matrix):
+	
 	N = len(matrix)
-	temp_array = [0]* N
 
-	for i in range(len(N/2)):
+	new_matrix = [] * N
+
+	for i in range(N):
+		new_matrix.append([0]*N)
+
+	for i in range(N):
 		for j in range(N):
-			temp_array = matrix[i][j]
-			matrix[i][j] = matrix[N-i-1][j]
-			matrix[N-i-1][j] = matrix[N-i-1][N-j-1]
-			matrix[N-i-1][N-j-1] = matrix[i][N-j-1]
-			matrix[i][N-j-1] = temp_array
+			new_matrix[j][N-i-1] = matrix[i][j]
+
+	return new_matrix
+
+'''
+	Approach 2: Rotating the matrix by in-place changing the values of the original matrix
+
+	Complexity: O(n * 2)
+'''
+
+# TODO : Implementation
+
+
+# Writing unit-test for the above function
+import unittest
+
+class MyTest(unittest.TestCase):
+
+	def test(self):
+		self.assertEqual(rotate_90([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]), [[13, 9, 5, 1], [14, 10, 6, 2], [15, 11, 7, 3], [16, 12, 8, 4]])
+
+unittest.main()
