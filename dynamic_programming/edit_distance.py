1bbf55f1d54f28bec0989b1d50c041a9944a77da
charulagrl <charul.agrl@gmail.com>
2017-05-01 00:47:01 +0530

    Find the minimum edit required to convert one string to another

--- dynamic_programming/edit_distance.py
+++ dynamic_programming/edit_distance.py
@@ -2,51 +2,70 @@
 
 # Edit distance 
 
-# Given two strings of size m, n and set of operations replace (R), insert (I) and delete (D) all at equal cost. Find minimum number of edits (operations) required to convert one string into another.
+''' 
+	Given two strings of size m, n and set of operations replace (R), insert (I) and delete (D) all at equal cost. Find minimum 
+	number of edits (operations) required to convert one string into another.
+'''
+import sys
 
-# Recursive solution
-
-def edit_distance(str1, str2, m, n):
-
-	if m == 0 or n == 0:
+def edit_distance(s1, s2):
+	''' Recursively find the minimum edit distance to convert string s1 to string s2'''
+	if len(s1) == 0 and len(s2) == 0:
 		return 0
 
-	if m == 0:
-		return n
+	if len(s1) == 0:
+		return len(s2)
 
-	if n == 0:
-		return m
+	if len(s2) == 0:
+		return len(s1)
 
-	upper_corner = edit_distance(str1, str2, m-1, n-1) + (str1[m-1] != str2[n-1])
-	left = edit_distance(str1, str2, m, n-1) + 1
-	down = edit_distance(str1, str2, m-1, n) + 1
+	if s1[0] == s2[0]:
+		return edit_distance(s1[1:], s2[1:])
 
-	return min(upper_corner, min(left, down))
+	else:
+		# If the first letters of two strings is not same, then we calculate the minimum edit distance in
+		# Insertion, Deletion or edit
+		return min(edit_distance(s1, s2[1:]), edit_distance(s1[1:], s2), edit_distance(s1[1:], s2[1:])) + 1
 
-def edit_distance_dynamic(str1, str2, m, n):
+def edit_distance_dynamic(s1, s2):
+	m = len(s1)
+	n = len(s2)
+	sol = [[sys.maxint] * (n+1) for i in range(m+1)]
 
-	soln = []
 	for i in range(m+1):
-		soln.append([0]*(n+1))
+		for j in range(n+1):
+			if i == 0 and j == 0:
+				sol[i][j] = 0
 
-	for i in range(m+1):
-		soln[i][0] = i
+			# When string s1 is of length 0, then it will insert all the letters of string s2
+			elif i == 0:
+				sol[i][j] = j
 
-	for j in range(n+1):
-		soln[0][j] = j
+			# If string s2 is of length 0, then it will delete all the letters of string s1
+			elif j == 0:
+				sol[i][j] = i
 
-	for i in range(1, m+1):
-		for j in range(1, n+1):
-			corner = soln[i-1][j-1] + (str1[i-1] != str2[j-1])
-			left = soln[i][j-1] + 1
-			down = soln[i-1][j] + 1
+			# If both the letters are same, then it will be same as previous edit distance. 
+			elif s1[i-1] == s2[j-1]:
+				sol[i][j] = sol[i-1][j-1]
+			
+			else:
+				sol[i][j] = min(sol[i][j-1], sol[i-1][j], sol[i-1][j-1]) + 1
 
-			soln[i][j] = min(corner, min(left, down))
+	return sol[m][n]  
 
-	return soln[m][n]
+import unittest
 
-str1 = "SUNR"
-str2 = "SUNUE"
-print edit_distance(str1, str2, len(str1), len(str2))
-print "Dynamic Programmin Solution"
-print edit_distance_dynamic(str1, str2, len(str1), len(str2))
+class MyTest(unittest.TestCase):
+	def setUp(self):
+		self.str1 = "SUNR"
+		self.str2 = "SUNUE"
+	
+	def test_edit_distance_recursive(self):
+		self.assertEqual(edit_distance(self.str1, self.str2), 2)
+
+	def test_edit_distance_dynamic(self):
+		self.assertEqual(edit_distance_dynamic(self.str1, self.str2), 2)
+
+if __name__ == "__main__":
+	unittest.main()
