4d223c22ceb8bf6b5aaaeabed5e358e61df14d97
charulagrl <charul.agrl@gmail.com>
2017-05-01 00:50:32 +0530

    Move the files to algorithms folder and make them classes

--- graph/algorithms/hamiltonian_path.py
+++ graph/algorithms/hamiltonian_path.py
@@ -11,9 +11,11 @@ from __future__ import print_function
 from data_structures.adjacency_list_graph import construct_graph
 
 class HamiltonianPath(object):
+	'''Find the hamiltonian path in a graph'''
 	def __init__(self, graph):
 		self.graph = graph
 		self.vertices = self.graph.adjacency_dict.keys()
+		self.hamiltonian_path = []
 
 	def is_hamitonian(self):
 		'''Check if the path exists between the current sequence of vertices'''
@@ -29,9 +31,7 @@ class HamiltonianPath(object):
 		'''Find all the permutation of vertices and return hamiltonian path if it exists'''
 		if (left == right):
 			if self.is_hamitonian():
-				for vertex in self.vertices:
-					print (vertex, end=" ")
-				print ("\n")
+				self.hamiltonian_path.append(self.vertices[:]) # Copying by value
 		else:
 			for i in range(left, right+1):
 				self.vertices[left], self.vertices[i] = self.vertices[i], self.vertices[left]
@@ -42,8 +42,16 @@ class HamiltonianPath(object):
 	def hamiltonian(self):
 		self.get_permutations(0, len(self.vertices)-1)
 
-if __name__ == "__main__":
+		return self.hamiltonian_path
 
+
+def test_hamiltonian():
 	g = construct_graph()
 	hamiltonian = HamiltonianPath(g)
-	hamiltonian.hamiltonian()
+	hamiltonian_path = hamiltonian.hamiltonian()
+	hamiltonian_path = [[str(vertex) for vertex in path] for path in hamiltonian_path]
+
+	print (hamiltonian_path)
+
+if __name__ == "__main__":
+	test_hamiltonian()
