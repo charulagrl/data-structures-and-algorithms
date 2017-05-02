ac44af476e27644c4d6f10142066092bd6dad414
charulagrl <charul.agrl@gmail.com>
2017-05-02 22:34:44 +0530

    Longest increasing subsequence

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -2,46 +2,54 @@
 
 # Program to find longest increasing subsequence (LIS)
 
+import sys
+max_lis_length = -sys.maxint
 
-max_length = 1
+def longest_increasing_subsequence(array, length):
+	'''Recursive implementation to find the length of longest increasing subsequence'''
+	global max_lis_length
 
-# Recursive solution
-def LIS(arr, n):
-
-	global max_length
-	if n == 1:
+	# If the array contains only one element, lis will be 1
+	if length == 1:
 		return 1
 
-	max_ending_here = 1
-	for i in range(1, n):
-		res = LIS(arr, i)
-		if arr[i-1] < arr[n-1] and res+1 > max_ending_here:
-			max_ending_here = res+1
+	current_lis_length = 1
+	
+	for i in range(1, length):
+		''' Find the length od lis for sub array till index i'''
+		subproblem_lis_length = longest_increasing_subsequence(array, i)
 
-	if max_ending_here > max_length:
-		max_length = max_ending_here
+		if (array[i-1] < array[length-1] and current_lis_length < subproblem_lis_length + 1):
+			current_lis_length = subproblem_lis_length + 1
 
-	return max_ending_here
+	if current_lis_length > max_lis_length:
+		max_lis_length = current_lis_length
 
-# Dynamic Programming solution
-def LIS_dynamic(arr):
-	n = len(arr)
+	return current_lis_length
 
-	lis = [1] * n
+def longest_increasing_subsequence_dynamic(array, length):
+	'''Using dynamic programming to find the length of longest increasing subsequence'''
+	lis = [1] * length
 
-	for i in range(1, n):
+	for i in range(length):
 		for j in range(i):
-			if arr[j] < arr[i] and lis[j] + 1 > lis[i]:
+			if (array[j] < array[i] and lis[i] < lis[j] + 1):
 				lis[i] = lis[j] + 1
-
+	
 	return max(lis)
 
+import unittest
 
-l = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+class MyTest(unittest.TestCase):
+	def setUp(self):
+		self.array = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+		self.length = len(self.array)
 
-LIS(l, len(l))
-print "Solving through dynammic programming"
-print LIS_dynamic(l)
-print "Solving through recursive way"
-print max_length
+	def test_longest_increasing_subsequence(self):
+		self.assertEqual(longest_increasing_subsequence(self.array, self.length), 6)
 
+	def test_longest_increasing_subsequence_dynamic(self):
+		self.assertEqual(longest_increasing_subsequence_dynamic(self.array, self.length), 6)
+
+if __name__ == "__main__":
+	unittest.main()
