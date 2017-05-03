b964e6b5eda478431b52c4e764d7121a43826155
charulagrl <charul.agrl@gmail.com>
2017-05-03 10:55:44 +0530

    Cut a rod to get a maximum value out of it

--- dynamic_programming/cutting_a_rod.py
+++ dynamic_programming/cutting_a_rod.py
@@ -2,34 +2,87 @@
 
 # Cutting a rod
 
-# Recursive implementation
-def cutting_a_rod(l, values):
-	if l <= 0:
+'''
+	Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n.
+	Determine the maximum value obtainable by cutting up the rod and selling the pieces.
+'''
+import sys
+
+
+def cutting_a_rod(length, values):
+	'''Recursive implementation to find the maximum values obtained by cutting a rod'''
+	# If length is less than or equal to 0
+	if length <= 0:
 		return 0
 
 	else:
-		max_val = -111111111
-		for i in range(l):
-			max_val = max(values[i] + cutting_a_rod(l-i-1, values), max_val)
+		max_val = -sys.maxint
+		for i in range(length):
+			# Cut the rod at different lengths
+			# length-i-1 because at index 0, length is 1
+			max_val = max(values[i] + cutting_a_rod(length-i-1, values), max_val)
 
 	return max_val
 
-# Dynamic Programming implementation
-def cutting_a_rod_dynamic(values):
 
-	soln = [0] * (len(values)+1)
+def cutting_a_rod_dynamic(values, length):
+	'''Dynamic Programming implementation to find the maximum values obtained by cutting a rod'''
+	soln = [0] * (length+1)
 
-	soln[0] = 0
-
-	for i in range(1, len(values)+1):
-		soln[i] = -11111
+	for i in range(1, length+1):
+		soln[i] = -sys.maxint
 		for j in range(i):
 			res = soln[i-j-1] + values[j]
 			if res > soln[i]:
 				soln[i] = res
 
-	return soln[len(values)]
+	return soln[length]
 
-arr = [1, 5, 8, 9, 10, 17, 17, 20]
-print cutting_a_rod(len(arr), arr)
-print cutting_a_rod_dynamic(arr)
+def cut_a_rod(index, price, length):
+	if not length or not price:
+		return 0
+
+	if length < index:
+		return cut_a_rod(index+1, price[1:], length)
+
+	else:
+		return max(cut_a_rod(index, price, length-index) + price[0], cut_a_rod(index+1, price[1:], length))
+
+def cut_a_rod_dynamic(price, length):
+	cost = [[0] * (length+1) for i in range(length+1)]
+
+	for i in range(length+1):
+		for j in range(length):
+			if not i or not j:
+				cost[i][j] = 0
+			elif i > j:
+				cost[i][j] = max(cost[i-j-1][j] + price[j-1], cost[i][j])
+			else:
+				cost[i][j] = cost[i][j-1]
+
+		print cost
+	return cost[length][length]
+
+
+import unittest
+
+class MyTest(unittest.TestCase):
+	def setUp(self):
+		self.price = [1, 5, 8, 9]
+		self.length = len(self.price)
+
+	def test_cutting_a_rod(self):
+		self.assertEqual(cutting_a_rod(self.length, self.price), 10)
+
+	def test_cuttin_a_rod_dynamic(self):
+		self.assertEqual(cutting_a_rod_dynamic(self.price, self.length), 10)
+
+	def test_cut_a_rod_recursive(self):
+		self.assertEqual(cut_a_rod(1, self.price, self.length), 10)
+
+	def test_cut_a_rod_dynamic(self):
+		self.assertEqual(cut_a_rod_dynamic(self.price, self.length), 10)
+
+if __name__ == "__main__":
+	unittest.main()
+
