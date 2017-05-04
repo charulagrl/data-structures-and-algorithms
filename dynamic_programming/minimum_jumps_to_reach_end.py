d90293e8ad479c6b0313d435cd1264098075a723
charulagrl <charul.agrl@gmail.com>
2017-05-04 13:59:05 +0530

    Minimum jumps to reach the end of stairs

--- dynamic_programming/minimum_jumps_to_reach_end.py
+++ dynamic_programming/minimum_jumps_to_reach_end.py
@@ -3,6 +3,26 @@
 # Minimum number of jumps to reach end
 
 import sys
+def minimum_step(steps_array, index, length):
+	'''Calculating the minimum number of steps to reach the top of stairs using recursion'''
+	if index == length-1:
+		return 0
+
+	if steps_array[index] == 0:
+		return sys.maxint
+
+	else:
+		steps = steps_array[index]
+		min_steps = sys.maxint
+		
+		for i in range(index+1, index+steps+1):
+			if i < length:
+				step = minimum_step(steps_array, i, length)
+
+			if step + 1 < min_steps:
+				min_steps = step + 1
+				
+		return min_steps
 
 # Recursive approach
 def min_jump(arr, start, end):
@@ -23,6 +43,22 @@ def min_jump(arr, start, end):
 
 	return minimum
 
+def minimum_step_dynamic(steps_array, length):
+	
+	if not length or not steps_array[0]:
+		return sys.maxint
+
+	soln = [sys.maxint] * length
+	soln[0] = 0
+	
+	for i in range(1, length):
+		for j in range(i):
+			if i <= steps_array[j] + j and steps_array[j] != sys.maxint:
+				soln[i] = min(soln[i], soln[j] + 1)
+
+	return soln[length-1]
+
+
 # Dynamic Programmic implementation
 def min_jump_dynamic(arr):
 	n = len(arr)
@@ -35,12 +71,24 @@ def min_jump_dynamic(arr):
 	for i in range(1, n):
 		soln[i] = sys.maxint
 		for j in range(i):
-				if i <= j + arr[j] and soln[j] != sys.maxint:
-					soln[i] = min(soln[i], soln[j]+1)
-					break
+			if i <= j + arr[j] and soln[j] != sys.maxint:
+				soln[i] = min(soln[i], soln[j]+1)
+				break
 
 	return soln[n-1]
 
-arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
-print min_jump(arr, 0, len(arr)-1)
-print min_jump_dynamic(arr)
+import unittest
+
+class MyTest(unittest.TestCase):
+	def setUp(self):
+		self.array = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
+		self.length = len(self.array)
+	
+	def test_minimum_step_recursive(self):
+		self.assertEqual(minimum_step(self.array, 0, self.length), 3)
+
+	def test_minimum_step_dynamic(self):
+		self.assertEqual(minimum_step_dynamic(self.array, self.length), 3)
+
+if __name__ == "__main__":
+	unittest.main()
